a26ff96d5cdc7dc8eb1d03529f3f774194a50500
grigode <griegocode@proton.me>
2025-04-07 15:59:23 -0500

    feat(package:icon): export iconify

--- packages/icon/src/index.ts
+++ packages/icon/src/index.ts
@@ -1,9 +1,10 @@
 import { App } from "vue";
+import * as Iconify from "@iconify/vue";
 import vgIcon from "./components/vg-icon.vue";
 
 export function install(app: App) {
   app.component("vg-icon", vgIcon);
 }
 
-export { vgIcon };
+export { vgIcon, Iconify };
 export type { PropType } from "vue";
